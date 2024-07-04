9acc43a8ef2ef585535bad9941038d9bbf90013e
nonzamo <nonzamomwende57@gmail.com>
2024-07-04 15:27:17 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+let noOfBlocks = 56;
+const permanentStreet = 42;
+const height =264;
+let start = 80;
+let destination = 34;
+
+function distanceFromHqinBlocks(){
+  console.log ((noOfBlocks)-(permanentStreet));
+  //returns the number of blocks from Hq
+}
+distanceFromHqinBlocks();
+
+function distanceFromHqInFeet (){
+console.log ([(noOfBlocks)-(permanentStreet)]*264);
+//return from distance fromhq in block is multiplied by block feet
+}
+distanceFromHqInFeet();
+
+function distanceTraveledInFeet (){
+console.log ([start-destination]*264);
+//return the distance travelled in feet
+}
+distanceTraveledInFeet();
